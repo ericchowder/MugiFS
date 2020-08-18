29426e8f22112132ffb2421c88388a1d6b8c8980
linksta7 <linksta7@gmail.com>
2020-08-18 13:00:50 -0400

    added signup route

--- server/models/user.js
+++ server/models/user.js
@@ -20,3 +20,5 @@ const userSchema = new mongoose.Schema({
 // Can be used anywhere since it is not explicitely exported
 // module.exports = mongoose.model("User", userSchema)
 mongoose.model("User", userSchema)
+// Access the User schema via below, example in auth.js:
+// const User = mongoose.model("User") 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const { restart } = require('nodemon')
+const User = mongoose.model("User") // Specify "User" from user.js schema
+
 
 router.get('/', (req, res) => {
     res.send("hello")
@@ -11,10 +15,39 @@ router.post('/signup', (req, res) => {
     // checking if all fields received from req.body
     if (!email || !password || !name){
         // status 422 is service understood req but cannot process
+        // returns if failed
         return res.status(422).json({error:"Please fill in all missing fields"})
     }
+    /* Originally used to show post request successfully reached back end
     res.json({message:"Successfully posted"})
     console.log(email)
+    */
+    // find user with body email matching schema email
+    User.findOne({email:email})
+    // with the returned informaiton (savedUser)
+    .then((savedUser) => {
+        // if savedUser exists (true), return error
+        if(savedUser){
+            return res.status(422).json({error:"user already exists with that email"})
+        }
+        const user = new User({
+            email, // shorthand for email:email
+            password, // shorthand for password:password
+            name //shorthand for name:name
+        })
+        user.save()
+        .then(user=>{
+            res.json({message:"saved succesfully"})
+        })
+        // log error if couldn't user.save
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    // log error if couldn't User.findOne()
+    .catch(err=>{
+        console.log(err)
+    })
 })
 
 module.exports = router
