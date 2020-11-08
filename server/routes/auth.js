58951f814e103378e5b39c8b3e00d4aa064b4161
linksta7 <linksta7@gmail.com>
2020-11-07 22:41:52 -0500

    created signin route and example route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,14 +3,24 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const { restart } = require('nodemon')
 const User = mongoose.model("User") // Specify "User" from user.js schema
+const bcrypt = require('bcrypt')
 
-
+// test route for auth root
 router.get('/', (req, res) => {
     res.send("hello")
 })
 
+// signup route
 router.post('/signup', (req, res) => {
     // storing values from post call
+    // Postman test query in raw JSON Body:
+    /*
+    {
+        "name":"myName",
+        "email":"myName@email.com",
+        "password":"myPassword1234"
+    }
+    */
     const {name,email,password} = req.body
     // checking if all fields received from req.body
     if (!email || !password || !name){
@@ -30,24 +40,66 @@ router.post('/signup', (req, res) => {
         if(savedUser){
             return res.status(422).json({error:"user already exists with that email"})
         }
-        const user = new User({
-            email, // shorthand for email:email
-            password, // shorthand for password:password
-            name //shorthand for name:name
-        })
-        user.save()
-        .then(user=>{
-            res.json({message:"saved succesfully"})
-        })
-        // log error if couldn't user.save
-        .catch(err=>{
-            console.log(err)
+        // number size (default of 10) determines security of password
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
+            // create user with provided details
+            const user = new User({
+                email, // shorthand for email:email
+                password:hashedpassword,
+                name //shorthand for name:name
+            })
+            // save created user
+            user.save()
+            .then(user=>{
+                res.json({message:"saved succesfully"})
+            })
+            // log error if couldn't user.save()
+            .catch(err=>{
+                console.log(err)
+            })
         })
     })
     // log error if couldn't User.findOne()
     .catch(err=>{
         console.log(err)
     })
+})
+
+// sign in route
+router.post('/signin',(req,res)=>{
+    // store received email and password
+    const {email,password} = req.body
+    // check both email and password are entered
+    if(!email || !password){
+        return res.status(422).json({error:"please add email or password"})
+    }
+    // find email within db
+    User.findOne({email:email})
+    // returned user saved into savedUser
+    .then(savedUser=>{
+        // if user doesn't exist in db, return error
+        if(!savedUser){
+            return res.status(422).json({error:"Invalid email or password"})
+        }
+        // compared received password with savedUser password
+        bcrypt.compare(password,savedUser.password)
+        // returns true or false in doMatch
+        .then(doMatch=>{
+            // respond successful if doMatch is true (password is correct)
+            if(doMatch){
+                res.json({message:"successfully signed in"})
+            }
+            // respond error if doMatch is false (password is incorrect)
+            else{
+                return res.status(422).json({error:"Invalid email or password"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+
 })
 
 module.exports = router
--- server/routes/exampleRoute.js
+++ server/routes/exampleRoute.js
@@ -2,8 +2,100 @@ const express = require('express')
 const router = express.Router()
 
 module.exports = () => {
+
+    const posts = [
+        { id: 1, name: 'Pos 1'},
+        { id: 2, name: 'Pos 2'},
+        { id: 3, name: 'Pos 3'},
+        { id: 4, name: 'Pos 4'},
+        { id: 5, name: 'Pos 5'},
+        { id: 6, name: 'Pos 6'},
+        { id: 7, name: 'Pos 7'},
+        { id: 8, name: 'Pos 8'},
+        { id: 9, name: 'Pos 9'},
+        { id: 10, name: 'Pos 10'},
+        { id: 11, name: 'Pos 11'},
+        { id: 12, name: 'Pos 12'}
+    ]
+
+    // test route
     router.get('/', (req, res) => {
         res.send("exampleRoute page")
     })
+    /*
+    // route for testing paginating
+    router.get('/paginate', (req, res) => {
+        // url should be "/example?page=1&limit=5"
+        // page value received from GET query for page # desired
+        const page = parseInt(req.query.page)
+        // amount value received from GET query for number of contracts to be listed
+        const amount = parseInt(req.query.amount)
+        // calculating start and end index for displaying contracts
+        const startIndex = (page - 1) * amount
+        const endIndex = page * amount
+        // object to store next and previous along with array
+        const results = {}
+        // object property "next" for next page
+        // if (endIndex < posts.length) here
+        results.next = {
+            page: page + 1,
+            amount: amount
+        }
+        // object property "prev" for prev page
+        // if (startIndex > 0) here
+        results.prev = {
+            page: page - 1,
+            amount: amount
+        }
+        // check for page limits
+        if(results.prev.page == 0){
+            results.prev.page = 1
+        }
+        // paginating
+        results.myPosts = posts.slice(startIndex, endIndex)
+        // api response
+        res.status(200).send({returnData: results})
+    })
+    */
+   // route for testing paginating
+    router.get('/paginate', paginatedResults(posts), (req, res) => {
+        res.status(200).send({returnData: res.paginatedResults})
+    })
     return router
+}
+
+// pagination as a middleware
+function paginatedResults(model) {
+    return (req, res, next) => {
+        // url should be "/example?page=1&limit=5"
+        // page value received from GET query for page # desired
+        const page = parseInt(req.query.page)
+        // amount value received from GET query for number of contracts to be listed
+        const amount = parseInt(req.query.amount)
+        // calculating start and end index for displaying contracts
+        const startIndex = (page - 1) * amount
+        const endIndex = page * amount
+        // object to store next and previous along with array
+        const results = {}
+        // object property "next" for next page
+        // if (endIndex < model.length) here
+        results.next = {
+            page: page + 1,
+            amount: amount
+        }
+        // object property "prev" for prev page
+        // if (startIndex > 0) here
+        results.prev = {
+            page: page - 1,
+            amount: amount
+        }
+        // check for page limits
+        if(results.prev.page == 0){
+            results.prev.page = 1
+        }
+        // paginating
+        results.myModel = model.slice(startIndex, endIndex)
+        res.paginatedResults = results
+        next()
+    }
 }
